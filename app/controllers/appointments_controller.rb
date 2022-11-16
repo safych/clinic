19cf29e1a503f3428a674b38aae06aaedf37495a
safych <v7lad@ukr.net>
2022-11-16 15:10:03 +0200

    Fixed a bug with adding doctor's recommendations

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,25 +1,20 @@
 class AppointmentsController < ApplicationController
   before_action :set_appointment, only: %i[ show edit update destroy ]
 
-  # GET /appointments or /appointments.json
   def index
     @appointments = Appointment.all
   end
 
-  # GET /appointments/1 or /appointments/1.json
   def show
   end
 
-  # GET /appointments/new
   def new
     @appointment = Appointment.new
   end
 
-  # GET /appointments/1/edit
   def edit
   end
 
-  # POST /appointments or /appointments.json
   def create
     @appointment = Appointment.new(appointment_params)
     verification = Appointment.find_by(patient_id: params[:appointment][:patient_id], date: @appointment.date, doctor_id: params[:appointment][:doctor_id])
@@ -45,17 +40,21 @@ class AppointmentsController < ApplicationController
   def add_recommendation
     @appointment = Appointment.find(params[:id])
     respond_to do |format|
-      if @appointment.update(recommendation: params[:recommendation], status: "done") && params[:recommendation] != ""
+      if params[:recommendation] != ""
+        @appointment.update(recommendation: params[:recommendation], status: "done")
         format.html { redirect_to appointment_url(@appointment), notice: "Appointment was successfully added to the recommendation." }
         format.json { render :show, status: :ok, location: @appointment }
       else
-        format.html { render :edit, status: :unprocessable_entity, notice: "Appointment wasn't successfully updated. Because recommendation empty." }
-        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { 
+          render json: @appointment.errors,
+          notice: "Appointment wasn't successfully updated. Because recommendation empty.", 
+          status: :unprocessable_entity 
+        }
       end
     end
   end
 
-  # DELETE /appointments/1 or /appointments/1.json
   def destroy
     @appointment.destroy
 
@@ -66,12 +65,10 @@ class AppointmentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_appointment
       @appointment = Appointment.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def appointment_params
       params.require(:appointment).permit(:doctor_id, :patient_id, :status, :recommendation, :date)
     end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
+  validates_length_of :recommendation, minimum: 5, allow_blank: true
+
   belongs_to :doctor
   belongs_to :patient
 end
