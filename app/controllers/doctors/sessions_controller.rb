1cd844fad1f45d7d685ef76d930986f4ad8b2dc5
safych <v7lad@ukr.net>
2023-02-14 13:35:49 +0200

    Removed extra code from templates

--- app/controllers/doctors/sessions_controller.rb
+++ app/controllers/doctors/sessions_controller.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 class Doctors::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
   def create
     self.resource = warden.authenticate(auth_options)
     set_flash_message!(:notice, :signed_in)
@@ -19,16 +11,4 @@ class Doctors::SessionsController < Devise::SessionsController
     flash[:notice] = "Error: Incorrect password or phone number!"
     redirect_to new_doctor_session_path
   end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
--- app/controllers/patients/sessions_controller.rb
+++ app/controllers/patients/sessions_controller.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 class Patients::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
   def create
     self.resource = warden.authenticate(auth_options)
     set_flash_message!(:notice, :signed_in)
@@ -18,16 +11,4 @@ class Patients::SessionsController < Devise::SessionsController
     flash[:notice] = "Error: Incorrect password or phone number!"
     redirect_to new_patient_session_path
   end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :surname, :phone, :age, :gender, :residence])
-  # end
 end
